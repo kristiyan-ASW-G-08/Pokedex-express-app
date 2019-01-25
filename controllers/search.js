86b0531870f8a1d0688210572a52d6084c9948ad
Kristiyan Todorov <kristiyantkirchev@gmail.com>
2019-01-25 19:46:10 +0200

    added validation

--- controllers/search.js
+++ controllers/search.js
@@ -1,14 +1,27 @@
+const { validationResult } = require('express-validator/check');
 exports.getSearchPage = (req, res, next) => {
   res.render('search', {
     path: '/search',
-    title: 'Search'
+    title: 'Search',
+    errorsArr:[],
+    errorMsg:null,
   });
 };
 exports.search = (req, res, next) => {
   const { name } = req.body;
-  const { subject } = req.body;
+  const { category } = req.body;
+  const errorsObj = validationResult(req);
+  const errorsArr = errorsObj.array()
+  if(errorsArr.length !== 0){
+    res.render('search', {
+      path: '/search',
+      title: 'Search',
+      errorsArr,
+      errorMsg:errorsArr[0].msg
+    });
+  }
   let redirectUrl;
-  switch (subject) {
+  switch (category) {
     case 'Pokemon':
       redirectUrl = `/pokemon/${name}`;
       break;
--- routes/search.js
+++ routes/search.js
@@ -1,10 +1,27 @@
 const path = require('path');
 
 const express = require('express');
-
+const { body } = require('express-validator/check');
 const searchController = require('../controllers/search');
 
 const router = express.Router();
 router.get('/search', searchController.getSearchPage);
-router.post('/search',searchController.search)
+router.post(
+  '/search',
+  [
+    body('name', `Name should  be atleast 2 letters long`)
+      .exists()
+      .escape()
+      .isLength({ min: 2 })
+      .isString()
+      .trim(),
+      body('category', 'Category should  be atleast 4 letters long')
+      .exists()
+      .escape()
+      .isLength({ min: 4 })
+      .isString()
+      .trim()
+  ],
+  searchController.search
+);
 module.exports = router;
